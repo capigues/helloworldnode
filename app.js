b71d0e7b0536242d437b9a280c7fd997a48a2063
Caleb <capigues@aggies.ncat.edu>
2020-05-12 19:16:43 -0400

    lots of changes again

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
+"use strict";
+exports.__esModule = true;
 var fs = require('fs');
 var path = require('path');
 var http = require('http');
 var server = http.createServer(function (req, res) {
-    fs.readFile(path.join(__dirname, 'public', 'index.html'), function (err, content) {
+    fs.readFile(path.join(__dirname, 'index.html'), function (err, content) {
         if (err)
             throw err;
         res.writeHead(200, { 'Content-Type': 'text/html' });
         res.end(content);
     });
 });
-server.listen(3000);
+var port = process.env.PORT || 3000;
+server.listen(port);
--- app.ts
+++ app.ts
@@ -1,17 +1,18 @@
+import { pathToFileURL } from "url";
 const fs = require('fs');
 const path = require('path');
 const http = require('http');
 
-const server = http.createServer((req,res) => {
 
-    fs.readFile(path.join(__dirname, 'public', 'index.html'), (err, content) => {
+
+const server = http.createServer((req,res) => {
+    fs.readFile(path.join(__dirname, 'index.html'), (err, content) => {
         if (err) throw err;
+
         res.writeHead(200, { 'Content-Type': 'text/html' });
         res.end(content);
-    })
+    });
 });
 
-
-
-
-server.listen(3000);
+const port = process.env.PORT || 3000;
+server.listen(port);
