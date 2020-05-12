7fcb84fdccda041b591a55895a545bdcaaaa1c9a
Caleb <capigues@aggies.ncat.edu>
2020-05-12 15:47:01 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,9 @@
+var Person = /** @class */ (function () {
+    function Person() {
+    }
+    return Person;
+}());
+var person1 = new Person();
+person1.age = 10;
+person1.name = 'Caleb';
+console.log(person1);
---
+++ app.ts
@@ -0,0 +1,11 @@
+class Person {
+    name: string;
+    age: number;
+}
+
+let person1 = new Person();
+
+person1.age = 10;
+person1.name = 'Caleb';
+
+console.log(person1);
